998dedc9fc147622a5bfe6f199237e39280a894b
Jakub Oźmina <jakub.ozmina@solidcompany.pl>
2025-08-26 19:17:19 +0200

    Added LICENSE Agreement

--- engine/src/FluxEngine.h
+++ engine/src/FluxEngine.h
@@ -1,3 +1,8 @@
 #pragma once
 
+// FluxEngine (FEL) v1.1
+// © 2025 Jakub Oźmina
+// For personal or educational use only. Commercial use requires author permission.
+// The author will try to provide support to anyone using this engine.
+
 #include "export.h"
